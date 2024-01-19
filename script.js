7a4e5475ba2eb71a592af15254a0d58189119430
VecchiR <79600745+VecchiR@users.noreply.github.com>
2024-01-19 18:38:48 -0300

    Add function to get CPU choice and sketch "whoWon()"
    Started to develop the whoWhon() function to compare the
    user's and CPU's choices to declare who won the match

--- script.js
+++ script.js
@@ -1,7 +1,61 @@
 let userChoice;
 let computerChoice;
+let winner;
 
 
 function startGame() {
-    console.log(userChoice);
+    console.log("user: ", userChoice);
+    console.log("cpu: ", getComputerChoice());
+    
+}
+
+function getComputerChoice() {
+    let randNum = (Math.floor(Math.random() * 100)) + 1;
+    switch (randNum>0) {
+        case (randNum >= 1 && randNum <= 30) :
+            computerChoice = "rock";
+            return computerChoice;
+            break;
+        case (randNum >= 31 && randNum <= 60) :
+            computerChoice = "paper";
+            return computerChoice;
+            break;
+        case (randNum >= 61) :
+            computerChoice = "scissors";
+            return computerChoice;
+            break;
+    }
+}
+
+function whoWon() {
+    switch (true) {
+        case (userChoice === computerChoice) :
+            winner = "draw";
+            return winner;
+            break;
+        case (userChoice === "rock" && computerChoice === "scissors") :
+            winner = "draw";
+            return winner;
+            break;
+        case (userChoice === "rock" && computerChoice === "paper") :
+            winner = "draw";
+            return winner;
+            break;
+        case (userChoice === "paper" && computerChoice === "scissors") :
+            winner = "draw";
+            return winner;
+            break;
+        case (userChoice === "paper" && computerChoice === "rock") :
+            winner = "draw";
+            return winner;
+            break;
+        case (userChoice === "scissors" && computerChoice === "rock") :
+            winner = "draw";
+            return winner;
+            break;
+        case (userChoice === "scissors" && computerChoice === "paper") :
+            winner = "draw";
+            return winner;
+            break;
+    }
 }
