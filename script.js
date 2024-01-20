286da518a5bb53c5ffb5e7d514a28fd22c6add29
VecchiR <79600745+VecchiR@users.noreply.github.com>
2024-01-20 16:58:31 -0300

    Make "winning a match" = "win 3 rounds"
    Now, to win a match, a player has to win 3 rounds.
    The game also shows the score after each round and, after a match,
    it displays who was the winner.
    If the user clicks another button after the match is ended, a new
    match will already begin.

--- script.js
+++ script.js
@@ -1,26 +1,66 @@
 let userChoice;
 let computerChoice;
 let winner;
+let userWinsCounter = 0;
+let computerWinsCounter = 0;
+let score ='';
+let gameOver = false;
+    
+
+function checkGameOver () {
+    if (userWinsCounter < 3 && computerWinsCounter < 3) {
+        gameOver = false;
+        return;
+    }
+
+    else {
+        gameOver = true;
+        return;
+    }
+}
+
 
 
 function startGame() {
     console.log("user: ", userChoice);
     console.log("cpu: ", getComputerChoice());
+
     whoWon();
+
     if (winner === "draw") {
         console.log("It's a DRAW!");
     }
+    
     else if (winner === "cpu") {
-        console.log("What a sad fate: YOU LOST!");
+        computerWinsCounter++;
+        console.log("cpu scores!");
     }
+    
     else if (winner === "user") {
-        console.log("Congratulations: YOU WON!");
+        userWinsCounter++;
+        console.log("you scored!");
     }
+    
     else {
         console.log("HOW THE F* DID YOU BREAK MY PROGRAM??");
     }
     
-    
+    score = `SCORE: USER (${userWinsCounter}) x (${computerWinsCounter}) CPU`;
+
+    console.log(score);
+    checkGameOver();
+
+   if (gameOver === true)
+   {
+        if (userWinsCounter > computerWinsCounter){
+            console.log("THE MACHINE IS DEFEATED! REJOICE! - YOU WON");
+        }
+        else if (computerWinsCounter > userWinsCounter){
+            console.log("WHAT A SAD FATE YOU ENCOUNTERED! - YOU LOST");
+        }
+        computerWinsCounter = 0;
+        userWinsCounter = 0;
+   }
 }
 
 function getComputerChoice() {
